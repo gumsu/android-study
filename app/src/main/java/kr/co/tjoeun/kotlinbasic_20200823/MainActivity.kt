91840bdc2736ea564aa0bb0c7881165b5b0534b1
gumsu <chavi9@naver.com>
2020-08-23 11:49:44 +0900

    토스트 띄워주기 체험

--- app/src/main/java/kr/co/tjoeun/kotlinbasic_20200823/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/kotlinbasic_20200823/MainActivity.kt
@@ -3,6 +3,7 @@ package kr.co.tjoeun.kotlinbasic_20200823
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,12 +13,17 @@ class MainActivity : AppCompatActivity() {
         
         clickMeBtn.setOnClickListener { 
 //            클릭미 버튼이 눌리면 실행될 코드 { }
-            
+
             Log.d("메인화면", "클릭 미 버튼 눌림")
         }
         makeLogBtn.setOnClickListener {
             Log.d("메인화면", "로그 남기기 버튼 눌림")
 //            xml에 추가한 버튼을 makeLogBtn으로 이름지어서 => 눌리면 "로그 남기기 버튼 눌림" 로그 남겨보기
+        }
+
+        toastBtn.setOnClickListener {
+//            사용자에게 "버튼 눌림" 안내 문구 제공
+            Toast.makeText(this, "버튼 눌림", Toast.LENGTH_SHORT).show()
         }
     }
 }
