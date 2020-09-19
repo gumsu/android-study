bb83b6e617fc83f83e2d22187b3f4e4e5d59b612
gumsu <chavi9@naver.com>
2020-09-19 11:32:56 +0900

    입력한 폰번에 DAIL 연결(전화 걸기 직전 상태)

--- app/src/main/java/kr/co/tjoeun/intent_20200823/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intent_20200823/MainActivity.kt
@@ -2,6 +2,7 @@ package kr.co.tjoeun.intent_20200823
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -39,6 +40,16 @@ class MainActivity : AppCompatActivity() {
 //            1000 대신 => 멤버변수로 만들어둔 REQUEST_FOR_NICKNAME를 활용, 가독성 향상
             startActivityForResult(myIntent,REQUEST_FOR_NICKNAME)
         }
+
+//        DIAL 액션 예제
+         dialBtn.setOnClickListener {
+//             입력한 폰번 받아오기
+             val inputPhoneNum = phoneNumEdt.text.toString()
+
+             val myUri = Uri.parse("tel:${inputPhoneNum}")
+             val myIntent = Intent(Intent.ACTION_DIAL,myUri)
+             startActivity(myIntent)
+         }
     }
 
     /*
