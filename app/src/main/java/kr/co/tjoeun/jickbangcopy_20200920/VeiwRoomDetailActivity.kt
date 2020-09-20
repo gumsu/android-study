500a02daecb9026f6bc5328a5689aea2830a1d53
gumsu <chavi9@naver.com>
2020-09-20 12:26:50 +0900

    방 상세화면 추가

---
+++ app/src/main/java/kr/co/tjoeun/jickbangcopy_20200920/VeiwRoomDetailActivity.kt
@@ -0,0 +1,20 @@
+package kr.co.tjoeun.jickbangcopy_20200920
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VeiwRoomDetailActivity : BasicActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_veiw_room_detail)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+        TODO("Not yet implemented")
+    }
+    override fun setValues() {
+        TODO("Not yet implemented")
+    }
+}
