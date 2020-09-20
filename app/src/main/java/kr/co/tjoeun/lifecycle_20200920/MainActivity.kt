0a2a5eea2b4594e2beae2c33df74d92ab62a74a6
gumsu <chavi9@naver.com>
2020-09-20 15:36:04 +0900

    onCreate 실행

--- app/src/main/java/kr/co/tjoeun/lifecycle_20200920/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/lifecycle_20200920/MainActivity.kt
@@ -1,11 +1,22 @@
 package kr.co.tjoeun.lifecycle_20200920
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        다른 화면을 갔다와도 또 실행되지 않는다. 재활용함
+        Log.d("메인화면","onCreate 실행됨")
+
+        moveToOtherBtn.setOnClickListener {
+            val myIntent = Intent(this,OtherActivity::class.java)
+            startActivity(myIntent)
+        }
     }
 }
