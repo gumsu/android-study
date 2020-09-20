0875e6c1b5609d6f4e5d991d9a5f0c07dead572e
gumsu <chavi9@naver.com>
2020-09-20 10:13:17 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/jickbangcopy_20200920/MainActivity.kt
@@ -0,0 +1,19 @@
+package kr.co.tjoeun.jickbangcopy_20200920
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BasicActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun setupEvents() {
+//        버튼 이벤트 / 리스트뷰 이벤트 등 이벤트처리만 모아두는 코드
+    }
+
+    override fun setValues() {
+//        .text 값 대입 등 화면 문구/데이터 반영하는 코드만.
+    }
+}
