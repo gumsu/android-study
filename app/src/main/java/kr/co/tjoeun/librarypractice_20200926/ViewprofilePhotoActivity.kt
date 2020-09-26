4585c473ca60f8b96bf2d86e0dd4aed1a4041755
gumsu <chavi9@naver.com>
2020-09-26 13:41:13 +0900

    프사 크게 보기 화면 추가  => 줌인 / 줌아웃 미구현

---
+++ app/src/main/java/kr/co/tjoeun/librarypractice_20200926/ViewprofilePhotoActivity.kt
@@ -0,0 +1,21 @@
+package kr.co.tjoeun.librarypractice_20200926
+
+import android.os.Bundle
+
+class ViewprofilePhotoActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_profile_view)
+
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
