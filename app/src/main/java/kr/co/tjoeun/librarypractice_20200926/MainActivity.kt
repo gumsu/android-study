5b8060b76b13a2a7a31ac01e394e6a38473b59ef
gumsu <chavi9@naver.com>
2020-09-26 15:21:53 +0900

    TedPermission 활용해서 CALL_PHONE 권한 승인 처리 (깃허브 설명 참고)

--- app/src/main/java/kr/co/tjoeun/librarypractice_20200926/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/librarypractice_20200926/MainActivity.kt
@@ -1,8 +1,13 @@
 package kr.co.tjoeun.librarypractice_20200926
 
+import android.Manifest
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -16,12 +21,29 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
-        loadWebImageButton.setOnClickListener {
+        callPhoneBtn.setOnClickListener {
 
+            val pl = object : PermissionListener{
+
+                override fun onPermissionGranted() {
+                    val phoneNum = phoneNumTxt.text.toString()
+
+                    val myUri = Uri.parse("tel:${phoneNum}")
+                    val myIntent = Intent(Intent.ACTION_CALL,myUri)
+                    startActivity(myIntent)
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(mContext, "권한이 거부되어 통화가 안됩니다", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            TedPermission.with(mContext)
+                .setPermissionListener(pl)
+                .setDeniedMessage("권한이 거부되었습니다. [설정]에서 권한을 켜주세요.")
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
         }
-    }
-
-    override fun setValues() {
 
         profilePhotoImg.setOnClickListener {
 //            Toast.makeText(mContext, "사진 클릭됨", Toast.LENGTH_SHORT).show()
@@ -32,5 +54,9 @@ class MainActivity : BaseActivity() {
         loadWebImageButton.setOnClickListener {
             Glide.with(mContext).load("https://img.sbs.co.kr/newimg/news/20200221/201405004_1280.jpg").into(otherPersonImg)
         }
+    }
+
+    override fun setValues() {
+
     }
 }
