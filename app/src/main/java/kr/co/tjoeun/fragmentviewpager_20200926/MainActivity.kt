36650f68ea3eea272925a4ac429d927e54df35fd
gumsu <chavi9@naver.com>
2020-09-26 10:51:27 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/fragmentviewpager_20200926/MainActivity.kt
@@ -0,0 +1,22 @@
+package kr.co.tjoeun.fragmentviewpager_20200926
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
