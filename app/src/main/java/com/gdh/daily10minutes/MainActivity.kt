1b9fc219a85191d00ff4545c80fd7a63d3a32150
gumsu <chavi9@naver.com>
2020-09-29 00:06:04 +0900

    로그인 클릭시 ID / PW 받아내기

--- app/src/main/java/com/gdh/daily10minutes/MainActivity.kt
+++ app/src/main/java/com/gdh/daily10minutes/MainActivity.kt
@@ -2,15 +2,22 @@ package com.gdh.daily10minutes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
     }
 
     override fun setupEvents() {
+        loginBtn.setOnClickListener {
 
+            val inputId = idEdt.text.toString()
+            val inputPw = pwEdt.text.toString()
+        }
     }
 
     override fun setValues() {
