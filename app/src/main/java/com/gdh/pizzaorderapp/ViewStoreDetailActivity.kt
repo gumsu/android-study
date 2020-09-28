971543d0309f1f7e1a70e777c16219afd9e078df
gumsu <chavi9@naver.com>
2020-09-28 22:57:20 +0900

    TedPermission으로 전화 걸기 처리

--- app/src/main/java/com/gdh/pizzaorderapp/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/gdh/pizzaorderapp/ViewStoreDetailActivity.kt
@@ -1,9 +1,15 @@
 package com.gdh.pizzaorderapp
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.gdh.pizzaorderapp.datas.Store
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import kotlinx.android.synthetic.main.activity_view_store_detail.*
 
 class ViewStoreDetailActivity : BaseActivity() {
@@ -19,6 +25,28 @@ class ViewStoreDetailActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+        callBtn.setOnClickListener {
+
+            val pl = object : PermissionListener{
+
+                override fun onPermissionGranted() {
+
+                    val myUri = Uri.parse("tel:${mStore.phoneNum}")
+                    val myIntent = Intent(Intent.ACTION_CALL,myUri)
+                    startActivity(myIntent)
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(mContext, "전화 연결 권한이 거부되어 통화가 불가능합니다", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            TedPermission.with(mContext)
+                .setPermissionListener(pl)
+                .setDeniedMessage("[설정]에서 전화 권한을 켜야 주문이 가능합니다")
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
+        }
     }
 
     override fun setValues() {
