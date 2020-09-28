597447a23ad5f7494207a27403a0e6955bd01ac2
gumsu <chavi9@naver.com>
2020-09-28 21:57:19 +0900

    Initial commit

---
+++ app/src/main/java/com/gdh/pizzaorderapp/BaseActivity.kt
@@ -0,0 +1,11 @@
+package com.gdh.pizzaorderapp
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    val mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+}
---
+++ app/src/main/java/com/gdh/pizzaorderapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.gdh.pizzaorderapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+    }
+}
