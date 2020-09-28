3c955ebad2f7619b54cd57df00bb073c05e7ab70
gumsu <chavi9@naver.com>
2020-09-28 22:28:46 +0900

    가게 목록 뿌리는 어댑터 뼈대작업

---
+++ app/src/main/java/com/gdh/pizzaorderapp/adapters/StoreAdapter.kt
@@ -0,0 +1,29 @@
+package com.gdh.pizzaorderapp.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.gdh.pizzaorderapp.R
+import com.gdh.pizzaorderapp.datas.Store
+
+class StoreAdapter(val mContext:Context,
+                   resId :Int,
+                   val mList:List<Store>) :ArrayAdapter<Store>(mContext,resId,mList){
+
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+
+        if(tempRow == null){
+            tempRow = inf.inflate(R.layout.store_list_item,null)
+        }
+        val row = tempRow!!
+
+        return row
+    }
+
+}
