2790c437f8f07a9ba52fa9b4ac7798efd9ae4674
gumsu <chavi9@naver.com>
2020-09-28 22:45:11 +0900

    가게 정보 상세 화면 추가

---
+++ app/src/main/java/com/gdh/pizzaorderapp/ViewStoreDetailActivity.kt
@@ -0,0 +1,20 @@
+package com.gdh.pizzaorderapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewStoreDetailActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_store_detail)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+    }
+}
