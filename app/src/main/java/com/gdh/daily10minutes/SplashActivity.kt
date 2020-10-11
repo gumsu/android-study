02e110972e3571996a2146012fc0fccf544f5aba
gumsu <chavi9@naver.com>
2020-10-11 11:09:10 +0900

    스플래쉬 화면 추가

---
+++ app/src/main/java/com/gdh/daily10minutes/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.gdh.daily10minutes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        setupEvents()
+        setValues()
+    }
+    override fun setupEvents() {
+    }
+    override fun setValues() {
+    }
+}
