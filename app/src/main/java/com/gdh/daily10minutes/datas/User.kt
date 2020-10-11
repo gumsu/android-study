2ee4eb0fa4a2b0d2a20df6998ee33f884691804f
gumsu <chavi9@naver.com>
2020-10-11 14:15:47 +0900

    사용자 데이터 클래스 추가와 파싱 기능도 같이 추가

---
+++ app/src/main/java/com/gdh/daily10minutes/datas/User.kt
@@ -0,0 +1,25 @@
+package com.gdh.daily10minutes.datas
+
+import org.json.JSONObject
+import java.io.Serializable
+
+class User :Serializable {
+
+    var id = 0
+    var email = ""
+    var nickName = ""
+
+    companion object {
+
+        fun getUserFromJSON(json:JSONObject) : User{
+
+            val u = User()
+
+            u.id = json.getInt("id")
+            u.email = json.getString("email")
+            u.nickName = json.getString("nick_name")
+
+            return u
+        }
+    }
+}
