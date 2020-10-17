c023af8341b64f261efbd1551dc0e4ba2b628f38
gumsu <chavi9@naver.com>
2020-10-17 10:12:44 +0900

    인증글 목록 화면 추가

---
+++ app/src/main/java/com/gdh/daily10minutes/ViewProofListActivity.kt
@@ -0,0 +1,19 @@
+package com.gdh.daily10minutes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewProofListActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_proof_list)
+        setupEvents()
+        setValues()
+    }
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+    }
+}
