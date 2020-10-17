dabd2ee070e26e97d9650db3d431b7015995779c
gumsu <chavi9@naver.com>
2020-10-17 11:26:19 +0900

    날짜 선택 이벤트 처리 (화면에 날짜 반영)

--- app/src/main/java/com/gdh/daily10minutes/ViewProofListActivity.kt
+++ app/src/main/java/com/gdh/daily10minutes/ViewProofListActivity.kt
@@ -1,5 +1,6 @@
 package com.gdh.daily10minutes
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -20,6 +21,26 @@ class ViewProofListActivity : BaseActivity() {
     }
     override fun setupEvents() {
 
+//        날짜 선택 버튼이 눌리면 => 달력을 띄워서 날짜 선택 받기
+        selectDateBtn.setOnClickListener {
+
+//            DatePickerDialog 활용
+            val dpd = DatePickerDialog(mContext, DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+
+//                선택된 년/월/일을 가지고 할 일 작성
+//                선택된 년월일 => 2020-05-06양식으로 가공해서 로그 출력
+                val selectedDate = Calendar.getInstance() //오늘 날짜가 들어있는 상태
+//                선택된 년월일로 일괄 적용
+                selectedDate.set(year,month,dayOfMonth)
+//                날짜 변환 양식 생성
+                val sdf = SimpleDateFormat("yyyy-MM-dd")
+//                Log.d("선택된 날짜",sdf.format(selectedDate.time))
+                selectedDateTxt.text = sdf.format(selectedDate.time)
+
+            }, 2020, Calendar.JUNE, 10) //기본값 설정(달력을 클릭했을 때 2020년6월10일이 기본으로 나온다)
+
+            dpd.show()
+        }
     }
 
     override fun setValues() {
