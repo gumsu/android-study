cd03599ea7a34d53d5c4e97601b2417aae83a55c
gumsu <chavi9@naver.com>
2020-10-17 15:50:04 +0900

    인증글 작성 화면 추가

---
+++ app/src/main/java/com/gdh/daily10minutes/EditProofActivity.kt
@@ -0,0 +1,18 @@
+package com.gdh.daily10minutes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditProofActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_proof)
+        setupEvents()
+        setValues()
+    }
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+    }
+}
