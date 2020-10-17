4ae543a9986a1c45eab2c828e413bc1a6cd76689
gumsu <chavi9@naver.com>
2020-10-17 10:18:21 +0900

    인증글 상세보기 진입

--- app/src/main/java/com/gdh/daily10minutes/ViewProofListActivity.kt
+++ app/src/main/java/com/gdh/daily10minutes/ViewProofListActivity.kt
@@ -2,8 +2,12 @@ package com.gdh.daily10minutes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gdh.daily10minutes.datas.Project
 
 class ViewProofListActivity : BaseActivity() {
+
+    lateinit var mProject : Project
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_proof_list)
@@ -15,5 +19,7 @@ class ViewProofListActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        mProject = intent.getSerializableExtra("project") as Project
     }
 }
