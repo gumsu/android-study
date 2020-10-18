4e9938988020ca513d1cef5d4674b2d8a7ddbc7c
gumsu <chavi9@naver.com>
2020-10-18 10:36:23 +0900

    인증글의 댓글 화면 추가

---
+++ app/src/main/java/com/gdh/daily10minutes/ViewProofReplyListActivity.kt
@@ -0,0 +1,22 @@
+package com.gdh.daily10minutes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewProofReplyListActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_proof_reply_list)
+        setupEvents()
+        setValues()
+
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
