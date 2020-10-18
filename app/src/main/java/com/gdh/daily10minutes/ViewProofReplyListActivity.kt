6f154e6071c6bd37b270e30992981f73c6934270
gumsu <chavi9@naver.com>
2020-10-18 12:18:40 +0900

    댓글 목록 불러오기 까지 진행 (실 데이터 반영 X)

--- app/src/main/java/com/gdh/daily10minutes/ViewProofReplyListActivity.kt
+++ app/src/main/java/com/gdh/daily10minutes/ViewProofReplyListActivity.kt
@@ -4,13 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.bumptech.glide.Glide
+import com.gdh.daily10minutes.dapters.ReplyAdapter
 import com.gdh.daily10minutes.datas.Proof
+import com.gdh.daily10minutes.datas.Reply
+import com.gdh.daily10minutes.utils.ServerUtil
 import com.gdh.daily10minutes.utils.TimeUtil
 import kotlinx.android.synthetic.main.activity_view_proof_reply_list.*
+import org.json.JSONObject
 
 class ViewProofReplyListActivity : BaseActivity() {
 
     lateinit var mProof : Proof
+
+    val mReplyArrayList = ArrayList<Reply>()
+    lateinit var mReplyAdapter : ReplyAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +28,7 @@ class ViewProofReplyListActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
 
     }
 
@@ -41,5 +49,29 @@ class ViewProofReplyListActivity : BaseActivity() {
         }
 
         writtenDateTimeTxt.text = TimeUtil.getTimeAgoByCalendar(mProof.proofTime)
+
+        mReplyAdapter = ReplyAdapter(mContext, R.layout.reply_list_item, mReplyArrayList)
+        replyListView.adapter = mReplyAdapter
+    }
+
+    fun getReplysFromServer(){
+
+        ServerUtil.getRequestReplyListByProofId(mContext, mProof.id, object :ServerUtil.JsonResponseHandler{
+            override fun onResponse(json: JSONObject) {
+                val dataObj = json.getJSONObject("data")
+                val proofObj = dataObj.getJSONObject("project")
+                val repliesArr = proofObj.getJSONArray("replies")
+
+                for(i in 0 until repliesArr.length()){
+//                    MainActivity의 Project 파싱 부분과 비교해보자(같은 기능)
+                    mReplyArrayList.add(Reply.getReplyFromJSON(repliesArr.getJSONObject(i)))
+                }
+
+//                목록 새로고침
+                runOnUiThread {
+                    mReplyAdapter.notifyDataSetChanged()
+                }
+            }
+        })
     }
 }
