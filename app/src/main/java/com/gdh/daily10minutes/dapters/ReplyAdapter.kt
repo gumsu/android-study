1d24ba19b902cb83f8dbcddbed8f22cbec8c357e
gumsu <chavi9@naver.com>
2020-10-18 12:05:28 +0900

    댓글 어댑터 뼈대 추가

---
+++ app/src/main/java/com/gdh/daily10minutes/dapters/ReplyAdapter.kt
@@ -0,0 +1,34 @@
+package com.gdh.daily10minutes.dapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.gdh.daily10minutes.R
+import com.gdh.daily10minutes.datas.Project
+import com.gdh.daily10minutes.datas.Reply
+
+class ReplyAdapter(val mContext:Context,
+                   resId:Int,
+                   val mList:List<Reply> ) : ArrayAdapter<Reply>(mContext,resId,mList) {
+
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+
+        if(tempRow == null){
+            tempRow = inf.inflate(R.layout.reply_list_item,null)
+        }
+
+        val row = tempRow!!
+
+
+
+        return row
+    }
+}
